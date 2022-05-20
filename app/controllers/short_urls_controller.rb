1b07da69069d8d53e12d7865637929014a6b4123
JoDaUT <jose.david.urena.torres@gmail.com>
2022-05-19 22:47:59 -0600

    feat(dev): Create and show controllers. Pending: Update title.

--- app/controllers/short_urls_controller.rb
+++ app/controllers/short_urls_controller.rb
@@ -1,20 +1,48 @@
+require 'json'
+include ShortUrlsHelper
+
 class ShortUrlsController < ApplicationController
 
-  # Since we're working on an API, we don't have authenticity tokens
   skip_before_action :verify_authenticity_token
+
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+  def not_found
+    render json: '{"error": "not_found"}', status: :not_found
+  end
 
   def index
     # return all short urls from database
-    # @short_urls = ShortUrl.all
-    # render json: @short_urls
-    render json:"hello world"
+    @short_urls = ShortUrl.all
+    render json: @short_urls.to_json
   end
 
   def create
+    params.require(:full_url)
+    full_url = params[:full_url]
+    short_url = ShortUrl.new
+    short_url.full_url = full_url
+    if !short_url.save
+      flash[:alert] = 'User was not saved'
+      return render json:{errors:short_url.errors.full_messages}, status: 400
+    end
+    short_code = short_url.short_code
+    return render json: { short_code: short_code, short_url: short_url}
+
   end
 
   def show
-    render json:"hello world2"
+    id = params[:id]
+    row = ShortUrlsHelper::base62Decoder(id)
+    short_url = nil
+    ShortUrl.transaction do
+      short_url = ShortUrl.lock(true).find(row)
+
+      short_url.click_count += 1
+      puts short_url.to_json
+      short_url.save!
+    end
+    redirect_to short_url.full_url
   end
 
 end
